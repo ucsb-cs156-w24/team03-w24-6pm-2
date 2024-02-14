515abc0226e0085bd869c721ca33fff3aa4ac4b3
Thienan Vu <thienanvu@umail.ucsb.edu>
2024-02-13 18:29:43 -0800

    Completed the RecReq Forms

--- frontend/src/tests/components/RecommendationRequests/RecommendationRequestsForm.test.js
+++ frontend/src/tests/components/RecommendationRequests/RecommendationRequestsForm.test.js
@@ -2,7 +2,7 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import RecommendationRequestsForm from "main/components/RecommendationRequests/RecommendationRequestsForm";
-import { recommendationRequestsFixtures, restaurantFixtures } from "fixtures/recommendationRequestsFixtures";
+import { recommendationRequestsFixtures } from "fixtures/recommendationRequestsFixtures";
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
