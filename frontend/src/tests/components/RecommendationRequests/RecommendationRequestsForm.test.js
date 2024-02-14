0f8298f28317f399fa0c6a54cee5408081699cb3
Thienan Vu <thienanvu@umail.ucsb.edu>
2024-02-14 15:41:00 -0800

    Fixed code to pass GitHub Tests

--- frontend/src/tests/components/RecommendationRequests/RecommendationRequestsForm.test.js
+++ frontend/src/tests/components/RecommendationRequests/RecommendationRequestsForm.test.js
@@ -14,88 +14,130 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe("RecommendationRequestsForm tests", () => {
-    const queryClient = new QueryClient();
+    test("renders correctly", async () => {
 
-    const expectedHeaders = ["Name", "Description"];
-    const testId = "RecommendationRequestsForm";
-
-    test("renders correctly with no initialContents", async () => {
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RecommendationRequestsForm />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <RecommendationRequestsForm />
+            </Router>
         );
+        await screen.findByText(/Requester Email/);
+        await screen.findByText(/Create/);
+    });
 
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    ttest("renders correctly when passing in a RecommendationRequest", async () => {
 
-        expectedHeaders.forEach((headerText) => {
-            const header = screen.getByText(headerText);
-            expect(header).toBeInTheDocument();
-        });
+        render(
+            <Router  >
+                <RecommendationRequestsForm initialContents={recommendationRequestsFixtures.oneRecommendation} />
+            </Router>
+        );
+        await screen.findByTestId(/RecommendationRequestsForm-id/);
+        expect(screen.getByText(/Id/)).toBeInTheDocument();
+        expect(screen.getByTestId(/RecommendationRequestsForm-id/)).toHaveValue("1");
+    });
+
+    ttest("Correct Error messsages on bad input", async () => {
+
+        render(
+            <Router  >
+                <Recomm />
+            </Router>
+        );
+        await screen.findByTestId("RecommendationRequestsForm-requesterEmail");
+        const requesterEmailField = screen.getByTestId("RecommendationRequestsForm-requesterEmail");
+        const professorEmailField = screen.getByTestId("RecommendationRequestsForm-professorEmail");
+        const explanationField = screen.getByTestId("RecommendationRequestsForm-explanation");
+        const dateRequestedField = screen.getByTestId("RecommendationRequestsForm-dateRequested");
+        const dateNeededField = screen.getByTestId("RecommendationRequestsForm-dateNeeded");
+        const doneField = screen.getByTestId("RecommendationRequestsForm-done");
+        const submitButton = screen.getByTestId("RecommendationRequestsForm-submit");
+
+        fireEvent.change(requesterEmailField, { target: { value: 'bad-input' } });
+        fireEvent.change(professorEmailField, { target: { value: 'bad-input' } });
+        fireEvent.change(explanationField, { target: { value: 'bad-input' } });
+        fireEvent.change(dateRequestedField, { target: { value: 'bad-input' } });
+        fireEvent.change(dateNeededField, { target: { value: 'bad-input' } });
+        fireEvent.change(doneField, { target: { value: 'bad-input' } });
+        fireEvent.click(submitButton);
+
+        //await screen.findByText(/QuarterYYYYQ must be in the format YYYYQ/);
+    });
+
+    test("Correct Error messsages on missing input", async () => {
+
+        render(
+            <Router  >
+                <RecommendationRequestsForm />
+            </Router>
+        );
+        await screen.findByTestId("RecommendationRequestsForm-submit");
+        const submitButton = screen.getByTestId("RecommendationRequestsForm-submit");
+
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/RequesterEmail is required./);
+        expect(screen.getByText(/ProfessorEmail is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Explanation is required./)).toBeInTheDocument();
+        expect(screen.getByText(/DateRequested is required./)).toBeInTheDocument();
+        expect(screen.getByText(/DateNeeded is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Done is required./)).toBeInTheDocument();
 
     });
 
-    test("renders correctly when passing in initialContents", async () => {
+    test("No Error messsages on good input", async () => {
+
+        const mockSubmitAction = jest.fn();
+
+
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RecommendationRequestsForm initialContents={recommendationRequestsFixtures.oneRecommendation} />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <RecommendationRequestsForm submitAction={mockSubmitAction} />
+            </Router>
         );
+        await screen.findByTestId("RecommendationRequestsForm-requesterEmail");
 
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+        
+        const requesterEmailField = screen.getByTestId("RecommendationRequestsForm-requesterEmail");
+        const professorEmailField = screen.getByTestId("RecommendationRequestsForm-professorEmail");
+        const explanationField = screen.getByTestId("RecommendationRequestsForm-explanation");
+        const dateRequestedField = screen.getByTestId("RecommendationRequestsForm-dateRequested");
+        const dateNeededField = screen.getByTestId("RecommendationRequestsForm-dateNeeded");
+        const doneField = screen.getByTestId("RecommendationRequestsForm-done");
+        const submitButton = screen.getByTestId("RecommendationRequestsForm-submit");
 
-        expectedHeaders.forEach((headerText) => {
-            const header = screen.getByText(headerText);
-            expect(header).toBeInTheDocument();
-        });
+        fireEvent.change(requesterEmailField, { target: { value: 'cgaucho@ucsb.edu' } });
+        fireEvent.change(professorEmailField, { target: { value: 'phtcon@ucsb.edu' } });
+        fireEvent.change(explanationField, { target: { value: 'BS/MS Program' } });
+        fireEvent.change(dateRequestedField, { target: { value: '2022-04-20T00:00:00' } });
+        fireEvent.change(dateNeededField, { target: { value: '2022-05-01T00:00:00' } });
+        fireEvent.change(doneField, { target: { value: 'false' } });
+        fireEvent.click(submitButton);
 
-        expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
-        expect(screen.getByText(`Id`)).toBeInTheDocument();
+        await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+        //expect(screen.queryByText(/QuarterYYYYQ must be in the format YYYYQ/)).not.toBeInTheDocument();
+        expect(screen.queryByText(/dateRequested must be in ISO format/)).not.toBeInTheDocument();
+        expect(screen.queryByText(/dateNeeded must be in ISO format/)).not.toBeInTheDocument();
+        
+        
+
     });
 
     test("that navigate(-1) is called when Cancel is clicked", async () => {
+
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RecommendationRequestsForm />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <RecommendationRequestsForm />
+            </Router>
         );
-        expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
-        const cancelButton = screen.getByTestId(`${testId}-cancel`);
+        await screen.findByTestId("RecommendationRequestsForm-cancel");
+        const cancelButton = screen.getByTestId("RecommendationRequestsForm-cancel");
 
         fireEvent.click(cancelButton);
 
         await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
-    });
 
-    test("that the correct validations are performed", async () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RecommendationRequestsForm />
-                </Router>
-            </QueryClientProvider>
-        );
-
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
-        const submitButton = screen.getByText(/Create/);
-        fireEvent.click(submitButton);
-
-        await screen.findByText(/Name is required/);
-        expect(screen.getByText(/Description is required/)).toBeInTheDocument();
-
-        const nameInput = screen.getByTestId(`${testId}-name`);
-        fireEvent.change(nameInput, { target: { value: "a".repeat(31) } });
-        fireEvent.click(submitButton);
-
-        await waitFor(() => {
-            expect(screen.getByText(/Max length 30 characters/)).toBeInTheDocument();
-        });
     });
 
 });
