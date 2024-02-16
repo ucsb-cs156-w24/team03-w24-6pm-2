90428ea28ac625ab28e85422d03fc31754dc4fc2
Mihir Bharti <mihirbharti@Mihirs-Laptop.local>
2024-02-16 14:10:10 -0800

    create page stuff

--- frontend/src/main/pages/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsCreatePage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsCreatePage.js
@@ -1,12 +1,47 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UCSBDiningCommonsMenuItemsForm from "main/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function UCSBDiningCommonsMenuItemsCreatePage() {
+export default function UCSBDiningCommonsMenuItemsCreatePage({storybook=false}) {
 
-  // Stryker disable all : placeholder for future implementation
+  const objectToAxiosParams = (ucsbDiningCommonsMenuItems) => ({
+    url: "/api/ucsbdiningcommonsmenuitems/post",
+    method: "POST",
+    params: {
+     diningCommonsCode: ucsbDiningCommonsMenuItems.diningCommonsCode,
+     name: ucsbDiningCommonsMenuItems.name,
+     station: ucsbDiningCommonsMenuItems.station
+    }
+  });
+
+  const onSuccess = (ucsbDiningCommonsMenuItems) => {
+    toast(`New UCSBDiningCommonsMenuItems Created - id: ${ucsbDiningCommonsMenuItems.id} diningCommonsCode: ${ucsbDiningCommonsMenuItems.diningCommonsCode}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/ucsbdiningcommonsmenuitems/all"] // mutation makes this key stale so that pages relying on it reload
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/ucsbdiningcommonsmenuitems" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New UCSBDiningCommonsMenuItems</h1>
+        <UCSBDiningCommonsMenuItemsForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   )
