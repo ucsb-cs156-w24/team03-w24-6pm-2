27046f1f996d35fec28b9eff21e1c4232810613a
Thienan Vu <124760099+thienanvuu@users.noreply.github.com>
2024-02-16 20:57:10 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,9 +19,6 @@ import RecommendationRequestsIndexPage from "main/pages/RecommendationRequests/R
 import RecommendationRequestsCreatePage from "main/pages/RecommendationRequests/RecommendationRequestsCreatePage";
 import RecommendationRequestsEditPage from "main/pages/RecommendationRequests/RecommendationRequestsEditPage";
 
-import UCSBOrganizationIndexPage from "main/pages/UCSBOrganization/UCSBOrganizationIndexPage";
-import UCSBOrganizationCreatePage from "main/pages/UCSBOrganization/UCSBOrganizationCreatePage";
-import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
 
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
@@ -88,29 +85,14 @@ function App() {
           hasRole(currentUser, "ROLE_USER") && (
             <>
               <Route exact path="/recommendationrequests" element={<RecommendationRequestsIndexPage />} />
-}
-
- {
+            </>
+          )
+        }
+        {
           hasRole(currentUser, "ROLE_ADMIN") && (
             <>
               <Route exact path="/recommendationrequests/edit/:id" element={<RecommendationRequestsEditPage />} />
-              <Route exact path="/recommendationrequests/create" element={<RecommendationRequestsCreatepage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/ucsborganization" element={<UCSBOrganizationIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_ADMIN") && (
-            <>
-      
-              <Route exact path="/ucsborganization/edit/:orgCode" element={<UCSBOrganizationEditPage />} />
-              <Route exact path="/ucsborganization/create" element={<UCSBOrganizationCreatePage />} />
+              <Route exact path="/recommendationrequests/create" element={<RecommendationRequestsCreatePage />} />
             </>
           )
         }
