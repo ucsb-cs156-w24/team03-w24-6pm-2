9fefe1b6ed9bb8ae61dafcadb2609371188557d5
Vincent Zhu <vincentzhu@ucsb.edu>
2024-02-18 06:25:37 -0800

    Articles: Edit, Test, Storybook Implmented

--- frontend/src/main/pages/Articles/ArticlesEditPage.js
+++ frontend/src/main/pages/Articles/ArticlesEditPage.js
@@ -1,13 +1,72 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import ArticlesForm from "main/components/Articles/ArticlesForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function ArticlesEditPage() {
+export default function ArticlesEditPage({storybook=false}) {
+  let { id } = useParams();
 
-  // Stryker disable all : articles for future implementation
+  const { data: articles, _error, _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/ucsbarticles?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/ucsbarticles`,
+        params: {
+          id
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (articles) => ({
+    url: "/api/ucsbarticles",
+    method: "PUT",
+    params: {
+      id: articles.id,
+    },
+    data: {
+      title: articles.title,
+      url: articles.url,
+      explanation: articles.explanation,
+      email: articles.email,
+      dateAdded: articles.dateAdded
+    }
+  });
+
+  const onSuccess = (articles) => {
+    toast(`Articles Updated - id: ${articles.id} title: ${articles.title}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/ucsbdarticles?id=${id}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/articles" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit Articles</h1>
+        {
+          articles && <ArticlesForm initialContents={articles} submitAction={onSubmit} buttonLabel="Update" />
+        }
       </div>
     </BasicLayout>
   )
 }
+
--- frontend/src/stories/components/Articles/ArticlesTable.stories.js
+++ frontend/src/stories/components/Articles/ArticlesTable.stories.js
@@ -18,25 +18,25 @@ const Template = (args) => {
 export const Empty = Template.bind({});
 
 Empty.args = {
-    dates: []
+    articles: []
 };
 
 export const ThreeItemsOrdinaryUser = Template.bind({});
 
 ThreeItemsOrdinaryUser.args = {
-    dates: articlesFixtures.threeArticles,
+    articles: articlesFixtures.threeArticles,
     currentUser: currentUserFixtures.userOnly,
 };
 
 export const ThreeItemsAdminUser = Template.bind({});
 ThreeItemsAdminUser.args = {
-    dates: articlesFixtures.threeArticles,
+    articles: articlesFixtures.threeArticles,
     currentUser: currentUserFixtures.adminUser,
 }
 
 ThreeItemsAdminUser.parameters = {
     msw: [
-        rest.delete('/api/articles', (req, res, ctx) => {
+        rest.delete('/api/ucsbarticles', (req, res, ctx) => {
             window.alert("DELETE: " + JSON.stringify(req.url));
             return res(ctx.status(200),ctx.json({}));
         }),
