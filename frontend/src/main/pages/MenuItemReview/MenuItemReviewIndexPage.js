25faba00c2b70b5103d253730e6182ffc344298e
Demir <demirince@ucsb.edu>
2024-02-18 22:04:44 -0800

    place holders coppied

---
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewIndexPage.js
@@ -0,0 +1,15 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function MenuItemReviewIndexPage() {
+
+  // Stryker disable all : MenuItemReview for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Index page not yet implemented</h1>
+        <p><a href="/MenuItemReview/create">Create</a></p>
+        <p><a href="/MenuItemReview/edit/1">Edit</a></p>
+      </div>
+    </BasicLayout>
+  )
+}
