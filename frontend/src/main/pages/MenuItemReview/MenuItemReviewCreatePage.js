861bf056564bcac11b640f172b14602d9eff8697
Demir <demirince@ucsb.edu>
2024-02-18 23:05:52 -0800

    create page done

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
@@ -1,12 +1,51 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemReviewForm from "main/components/MenuItemReview/MenuItemReviewForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemReviewCreatePage() {
+export default function MenuItemReviewCreatePage({storybook=false}) {
 
-  // Stryker disable all : MenuItemReview for future implementation
+  const objectToAxiosParams = (review) => ({
+    url: "/api/menuitemreview/post",
+    method: "POST",
+    params: {
+      itemID: review.itemId,
+      reviewerEmail: review.reviewerEmail,
+      localDateTime: review.localDateTime,
+      stars: review.stars,
+      comments: review.comments
+    }
+  });
+
+  const onSuccess = (review) => {
+    toast(`New review Created - id: ${review.id}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/menuitemreview/all"]
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuitemreview" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Review</h1>
+
+        <MenuItemReviewForm submitAction={onSubmit} />
+
       </div>
     </BasicLayout>
   )
